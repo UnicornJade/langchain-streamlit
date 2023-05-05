0ae07b393bef809414bf4ab146b4c9f8d829e5fa
Ying <75564111+UnicornJade@users.noreply.github.com>
2023-05-05 21:07:35 +0800

    Update openai_template.py

--- openai_template.py
+++ openai_template.py
@@ -23,7 +23,7 @@ def translate(lang, text):
     return chain.run(text).strip()
 
 def planMaker(content):
-    template = """You are a coach. You will be responsible to me to the end. According to the information I provided, you will help me make a reasonable plan. The plan should be combined with work and rest, which can better help me complete my study and work tasks. Your answer should be as short and clear as possible and in Chinese.Here are my latest plans：'{content}'"""
+    template = """You are a coach. You will be responsible to me to the end. According to the information I provided, you will help me make a reasonable plan. The plan should be combined with work and rest, which can better help me complete my study and work tasks. Your answer should be as short and clear as possible and in Chinese.同时每隔一段文字自动换行\n显示,保证样式漂亮。Here are my latest plans：'{content}'"""
     llm = OpenAI(temperature=0.9)
     prompt = PromptTemplate(
         input_variables=["content"],  # 提示词·可控变量
