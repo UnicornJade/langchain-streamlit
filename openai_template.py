173f06228c6e15466eb68bcd695882ed63d2f776
Ying <75564111+UnicornJade@users.noreply.github.com>
2023-05-05 20:27:46 +0800

    Update openai_template.py

--- openai_template.py
+++ openai_template.py
@@ -7,9 +7,9 @@ from langchain.prompts import PromptTemplate
 """----------------------------------------------------------------"""
 
 
-def translate(lang, text):
-    en2zh = "Please translate english to chinese:{text}"
-    zh2en = "Please translate chinese to english:{text}"
+def translate(lang, role,text):
+    en2zh = "Hi,my friend {role}.Please translate english to chinese:{text}"
+    zh2en = "Hi,my friend {role}.Please translate chinese to english:{text}"
     if lang == 'English':
         template = zh2en
     elif lang == '简体中文':
@@ -17,8 +17,8 @@ def translate(lang, text):
 
     llm = OpenAI(temperature=0)
     prompt = PromptTemplate(
-        input_variables=["text"],  # 提示词·可控变量
+        input_variables=["role","text"],  # 提示词·可控变量
         template=template,
     )
     chain = LLMChain(llm=llm, prompt=prompt)
-    return chain.run(text).strip()
+    return chain.run(role,text).strip()
