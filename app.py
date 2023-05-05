6911fad1108f8f9dc974a020293799d0438e6c32
Ying <75564111+UnicornJade@users.noreply.github.com>
2023-05-05 19:06:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,46 +21,46 @@ with col1:
 
 """)
 
-with col2:
-    # st.markdown(
-    #     """
-    #     <style>
-    #     img {
-    #         border-radius: 6%;
-    #     }
-    #     </style>
-    #     """,
-    #     unsafe_allow_html=True,
-    # )
-    #
-    # st.image(
-    #     image="/Users/jadeunicorn/Pictures/midjourney/my pic/out/赛博girl/shill_No_Prompt_8922bab7-48a1-4a14-829e-916589a14e04.png",
-    # )
+# with col2:
+#     # st.markdown(
+#     #     """
+#     #     <style>
+#     #     img {
+#     #         border-radius: 6%;
+#     #     }
+#     #     </style>
+#     #     """,
+#     #     unsafe_allow_html=True,
+#     # )
+#     #
+#     # st.image(
+#     #     image="/Users/jadeunicorn/Pictures/midjourney/my pic/out/赛博girl/shill_No_Prompt_8922bab7-48a1-4a14-829e-916589a14e04.png",
+#     # )
 
-    df = pd.DataFrame({
-        '姓名': ['张三', '李四', '王五'],
-        '年龄': [22, 25, 28],
-        '性别': ['男', '男', '女']
-    })
+#     df = pd.DataFrame({
+#         '姓名': ['张三', '李四', '王五'],
+#         '年龄': [22, 25, 28],
+#         '性别': ['男', '男', '女']
+#     })
 
-    # 在 Streamlit 中显示表格
-    st.write(df)
+#     # 在 Streamlit 中显示表格
+#     st.write(df)
 
-    # 定义下载函数
-    def download_link(object_to_download, download_filename, download_link_text, icon='cloud-download-alt'):
-        if isinstance(object_to_download,pd.DataFrame):
-            object_to_download = object_to_download.to_csv(index=False)
-        # Some strings <-> bytes conversions necessary here
-        b64 = base64.b64encode(object_to_download.encode()).decode()
-        icon_html = f'<i class="fas fa-{icon}" style="padding-right: 5px;"></i>'
-        href = f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{icon_html}{download_link_text}</a>'
-        return href
+#     # 定义下载函数
+#     def download_link(object_to_download, download_filename, download_link_text, icon='cloud-download-alt'):
+#         if isinstance(object_to_download,pd.DataFrame):
+#             object_to_download = object_to_download.to_csv(index=False)
+#         # Some strings <-> bytes conversions necessary here
+#         b64 = base64.b64encode(object_to_download.encode()).decode()
+#         icon_html = f'<i class="fas fa-{icon}" style="padding-right: 5px;"></i>'
+#         href = f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{icon_html}{download_link_text}</a>'
+#         return href
 
-    # 添加下载按钮
-    st.markdown(download_link(df, 'data.csv', '下载 CSV 文件', 'download'), unsafe_allow_html=True)
+#     # 添加下载按钮
+#     st.markdown(download_link(df, 'data.csv', '下载 CSV 文件', 'download'), unsafe_allow_html=True)
 
-    # 添加 Font Awesome CSS 样式
-    st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-nWvIHYPKxstvZXctI5Sz4aHwq/U2tCVfQSQJ3cwR6y/QTSeSy2IWzsgCUpRIav4+AJ89P4KjLkarNp1gB5wUw==" crossorigin="anonymous" referrerpolicy="no-referrer" />', unsafe_allow_html=True)
+#     # 添加 Font Awesome CSS 样式
+#     st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-nWvIHYPKxstvZXctI5Sz4aHwq/U2tCVfQSQJ3cwR6y/QTSeSy2IWzsgCUpRIav4+AJ89P4KjLkarNp1gB5wUw==" crossorigin="anonymous" referrerpolicy="no-referrer" />', unsafe_allow_html=True)
 
 st.markdown('-----')
 
